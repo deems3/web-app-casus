bb93774c8bae510fb0171871a7536a986e5bf68b
MartijnW <weningmartijn@gmail.com>
2024-10-17 13:32:23 +0200

    Products

---
+++ src/Victuz_MVC/Migrations/20241011123509_ItemsInDatabase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Victuz_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemsInDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ProductCategory",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Testproductcategorie 1" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "Name", "Price" },
+                values: new object[] { 1, 1, "Het eerste testproduct1", "Testproduct 1", 19.99m });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategory",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
