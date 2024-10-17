b7bfafbc28947c8662e9e8e9f26f303d53205e56
aaron514 <134741769+aaron514@users.noreply.github.com>
2024-10-17 12:50:21 +0200

    # Product klasse - Annotations aangepast - Properties aangepast

--- src/Victuz_MVC/Models/Product.cs
+++ src/Victuz_MVC/Models/Product.cs
@@ -6,14 +6,13 @@ namespace Victuz_MVC.Models
     {
         [Key]
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public decimal Price { get; set; }
-
-
-        public int CategoryId { get; set; }
 
         [Required]
-        public ProductCategory Category { get; set; } = null!;
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
     }
 }
