1b0f1e9c144785c99fecc3c6f488044b08208c87
MeesHvnrs <78762910+MeesHvnrs@users.noreply.github.com>
2024-10-18 10:07:59 +0200

    error category

--- src/Victuz_MVC/Data/ApplicationDbContext.cs
+++ src/Victuz_MVC/Data/ApplicationDbContext.cs
@@ -19,26 +19,45 @@ namespace Victuz_MVC.Data
         {
         }
 
+
+        // Vaste Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+
+            // Category data
             ProductCategory productCategory1 = new ProductCategory
             {
                 Id = 1,
-                Name = "Testproductcategorie 1",
+                Name = "T-Shirt",
             };
+
+            ProductCategory productCategory2 = new ProductCategory
+            {
+                Id = 2,
+                Name = "Rood",
+            };
+
+            ProductCategory productCategory3 = new ProductCategory
+            {
+                Id = 3,
+                Name = "Zwart",
+            };
+
+            // Product data
 
             Product product1 = new Product
             {
                 Id = 1,
-                Name = "Testproduct 1",
-                Description = "Het eerste testproduct",
-                Price = 19.99m,
-                //CategoryId = 1
-            };
+                Name = "T-Shirt",
+                Description = "Dit is een t-shirt",
+                Price = 10.00M,
+            };  
 
             builder.Entity<ProductCategory>().HasData(productCategory1);
+            builder.Entity<ProductCategory>().HasData(productCategory2);
+            builder.Entity<ProductCategory>().HasData(productCategory3);
             builder.Entity<Product>().HasData(product1);
         }
     }
